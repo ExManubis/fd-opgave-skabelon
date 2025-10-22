4bd034b3f33f6c2d62429487d96649b22b2ccec6
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2025-10-22 14:34:51 +0200

    feath(server): add server side rendering
    Added netlify integration for server side rendering.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,33 +3,38 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-	prefetch: {
-		prefetchAll: true,
-		defaultStrategy: 'viewport',
+  prefetch: {
+      prefetchAll: true,
+      defaultStrategy: 'viewport',
 	},
 
-	image: {
-		responsiveStyles: true,
-		layout: 'constrained',
+  image: {
+      responsiveStyles: true,
+      layout: 'constrained',
 	},
 
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
-	experimental: {
-		fonts: [
-			{
-				provider: fontProviders.google(),
-				name: 'Cabin',
-				cssVariable: '--font-cabin',
-			},
-			{
-				provider: fontProviders.google(),
-				name: 'Lato',
-				cssVariable: '--font-lato',
-			},
-		],
+
+  experimental: {
+      fonts: [
+          {
+              provider: fontProviders.google(),
+              name: 'Cabin',
+              cssVariable: '--font-cabin',
+          },
+          {
+              provider: fontProviders.google(),
+              name: 'Lato',
+              cssVariable: '--font-lato',
+          },
+      ],
 	},
+
+  adapter: netlify(),
 });
